19df9e30d520e47fe6ece9c2bbb281b9d19aa96c
David Álvarez Pons <david_alvarezpons@hotmail.com>
2019-11-17 19:11:07 +0100

    Rename expected variables

--- test/Matchers/StringMatcherTest.cpp
+++ test/Matchers/StringMatcherTest.cpp
@@ -9,26 +9,26 @@ TEST_CASE("StringMatcher matches only with same strings")
     StringMatcher m1("a"), m2("b"), m3("x");
     forward_list<MatchableInterface*>
         input{&a, &b, &c},
-        expectedMatchedM1{&a},
-        expectedNotMatchedM1{&b, &c},
-        expectedMatchedM2{&a, &b},
-        expectedNotMatchedM2{&c},
-        expectedMatchedM3{&a, &b, &c},
-        expectedNotMatchedM3{};
+        expectedConsumedM1{&a},
+        expectedPendingM1{&b, &c},
+        expectedConsumedM2{&a, &b},
+        expectedPendingM2{&c},
+        expectedConsumedM3{&a, &b, &c},
+        expectedPendingM3{};
     map<string, forward_list<MatchableInterface*> > expectedOutputs;
 
     Result r1 = m1.match(input).front();
-    REQUIRE(r1.getConsumed() == expectedMatchedM1);
-    REQUIRE(r1.getPending() == expectedNotMatchedM1);
+    REQUIRE(r1.getConsumed() == expectedConsumedM1);
+    REQUIRE(r1.getPending() == expectedPendingM1);
     REQUIRE(r1.getOutputs() == expectedOutputs);
 
     Result r2 = m2.match(input).front();
-    REQUIRE(r2.getConsumed() == expectedMatchedM2);
-    REQUIRE(r2.getPending() == expectedNotMatchedM2);
+    REQUIRE(r2.getConsumed() == expectedConsumedM2);
+    REQUIRE(r2.getPending() == expectedPendingM2);
     REQUIRE(r2.getOutputs() == expectedOutputs);
 
     Result r3 = m3.match(input).front();
-    REQUIRE(r3.getConsumed() == expectedMatchedM3);
-    REQUIRE(r3.getPending() == expectedNotMatchedM3);
+    REQUIRE(r3.getConsumed() == expectedConsumedM3);
+    REQUIRE(r3.getPending() == expectedPendingM3);
     REQUIRE(r3.getOutputs() == expectedOutputs);
 }
