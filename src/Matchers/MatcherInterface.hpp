93af2e7ffd5bee85c6b3309452efe8ab9f37b57d
David Álvarez Pons <david_alvarezpons@hotmail.com>
2019-11-23 12:29:41 +0100

    Add comment explaining about vectors

--- src/Matchers/MatcherInterface.hpp
+++ src/Matchers/MatcherInterface.hpp
@@ -8,6 +8,11 @@ using namespace std;
 class MatcherInterface
 {
 public:
+    /**
+     * A vector is used for matchables due to the fact that some matchers may
+     * need to iterate backwards to resolve the match.
+     * For example: lookbehind (positive, negative, conditional)
+     */
     virtual forward_list<Result> match(
         vector<MatchableInterface *> matchables,
         int start
