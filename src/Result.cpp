3c9c9801eb2c0df91e704fdb800c3bae8c7abbb6
David Álvarez Pons <david_alvarezpons@hotmail.com>
2019-12-06 20:18:20 +0100

    Implement operator<< for Result

--- src/Result.cpp
+++ src/Result.cpp
@@ -1,5 +1,7 @@
 #include "Result.hpp"
 
+using namespace std;
+
 Result::Result(int index)
 {
     lastMatchedIndex = index;
@@ -33,4 +35,19 @@ map<string, forward_list<MatchableInterface *>> Result::getOutputs() const
 void Result::setOutputs(const map<string, forward_list<MatchableInterface *>> &outs)
 {
     outputs = outs;
+}
+
+ostream& operator<<(ostream &out, const Result &r)
+{
+    out << "Result(" << r.getLastMatchedIndex() << ", {" << endl;
+    auto outputs = r.getOutputs();
+    for (auto it = outputs.begin(); it != outputs.end(); ++it) {
+        out << "\t\"" << it->first << "\": [";
+        for (MatchableInterface* m: it->second) {
+            out << m->getValue() << ", ";
+        }
+        out << "]" << endl;
+    }
+    out << "})" << endl;
+    return out;
 }
--- src/Result.hpp
+++ src/Result.hpp
@@ -3,7 +3,10 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <ostream>
 #include "Matchables/MatchableInterface.hpp"
+
+using namespace std;
 
 class Result
 {
@@ -19,6 +22,8 @@ public:
     map<string, forward_list<MatchableInterface *>> getOutputs() const;
 
     void setOutputs(const map<string, forward_list<MatchableInterface *>> &outputs);
+
+    friend ostream& operator<<(ostream& out, const Result& r);
 
 protected:
     int lastMatchedIndex;
