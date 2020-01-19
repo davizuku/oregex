09464baa5188fddfb516ff2438d7de8624a3f4a6
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-01-19 19:24:39 +0100

    Add test provider for performance tests

--- test/PerformanceTest.cpp
+++ test/PerformanceTest.cpp
@@ -13,37 +13,56 @@
 
 using namespace std;
 
+struct TestArgs
+{
+    string name;
+    string strInput;
+    regex strRegex;
+    vector<MatchableInterface *> oInput;
+    Oregex oRegex;
+};
+
 TEST_CASE("Given the same set of strings how much faster is regex module")
 {
-    string strInput = "abccded";
-    regex strRegex("ab(c*)d((ed)*)");
     StringMatchable a("a"), b("b"), c("c"), d("d"), e("e");
     StringMatcher m1("a"), m2("b"), m3("x"), m4("c"), m5("d"), m6("e");
     StarMatcher s1(&m1), s2(&m2), s3(&m3), s4(&m4), s5(&m5);
-    vector<MatchableInterface *> matchableInput{&a, &b, &c, &c, &d, &e, &d};
-    Oregex oRegex(vector<MatcherInterface *>{
-        &m1,
-        &m2,
-        new GroupMatcher(vector<MatcherInterface *>{&s4}),
-        &m5,
-        new GroupMatcher(vector<MatcherInterface *>{
-            new StarMatcher(
-                new GroupMatcher(vector<MatcherInterface *>{&m6, &m5})
-            )
-        })
-    });
-    float tRegex = 0, tOregex = 0;
-    unsigned int nIterations = 100;
-    for (unsigned i = 0; i < nIterations; ++i) {
-        clock_t t0 = clock();
-        bool r1 = regex_match(strInput, strRegex);
-        clock_t t1 = clock();
-        bool r2 = oRegex.match(matchableInput);
-        clock_t t2 = clock();
-        REQUIRE(r1 == r2);
-        tRegex += (float)(t1 - t0) / CLOCKS_PER_SEC;
-        tOregex += (float)(t2 - t1) / CLOCKS_PER_SEC;
+
+    auto testProvider = vector<TestArgs>{
+        TestArgs{
+            "String 'abccded' into regex '/ab(c*)d((ed)*)/'",
+            "abccded",
+            regex("ab(c*)d((ed)*)"),
+            vector<MatchableInterface *>{&a, &b, &c, &c, &d, &e, &d},
+            Oregex(vector<MatcherInterface *>{
+                &m1,
+                &m2,
+                new GroupMatcher(vector<MatcherInterface *>{&s4}),
+                &m5,
+                new GroupMatcher(vector<MatcherInterface *>{
+                    new StarMatcher(
+                        new GroupMatcher(vector<MatcherInterface *>{&m6, &m5})
+                    )
+                })
+            })
+        }
+    };
+
+    for (TestArgs &testCase : testProvider) {
+        float tRegex = 0, tOregex = 0;
+        unsigned int nIterations = 100;
+        for (unsigned i = 0; i < nIterations; ++i) {
+            clock_t t0 = clock();
+            bool r1 = regex_match(testCase.strInput, testCase.strRegex);
+            clock_t t1 = clock();
+            bool r2 = testCase.oRegex.match(testCase.oInput);
+            clock_t t2 = clock();
+            REQUIRE(r1 == r2);
+            tRegex += (float)(t1 - t0) / CLOCKS_PER_SEC;
+            tOregex += (float)(t2 - t1) / CLOCKS_PER_SEC;
+        }
+        cout << endl << "Test: " << testCase.name << endl;
+        cout << "\tAverage time std regex: " << tRegex / nIterations << endl;
+        cout << "\tAverage time oregex: " << tOregex / nIterations << endl;
     }
-    cout << "Average time std regex: " << tRegex / nIterations << endl;
-    cout << "Average time oregex: " << tOregex / nIterations << endl;
 }
