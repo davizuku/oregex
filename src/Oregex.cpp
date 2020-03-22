5397c820acf29b52ef06187bd79e5e5862689730
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-03-22 12:15:44 +0100

    Avoid computing unnecessary results

--- src/Oregex.cpp
+++ src/Oregex.cpp
@@ -17,21 +17,6 @@ bool Oregex::match(vector<MatchableInterface *> &matchables)
     return match(matchables, outputs);
 }
 
-void buildOutputs(
-    unordered_map<string, forward_list<MatchableInterface *>> &outputs,
-    const list<Result*> &results
-) {
-    for (const Result* r: results) {
-        auto rOuts = r->getOutputs();
-        for (auto it = rOuts.begin(); it != rOuts.end(); ++it) {
-            bool hasKey = outputs.find(it->first) != outputs.end();
-            if (not hasKey) {
-                outputs[it->first] = it->second;
-            }
-        }
-    }
-}
-
 bool Oregex::match(
     vector<MatchableInterface *> &matchables,
     unordered_map<string, forward_list<MatchableInterface *>> &outputs
@@ -39,12 +24,8 @@ bool Oregex::match(
     for (size_t i = 0; i < matchables.size(); ++i) {
         list<Result*> results;
         Result* r = matcher->match(matchables, i);
-        while (r != NULL) {
-            results.push_back(r);
-            r = matcher->next();
-        }
-        if (not results.empty()) {
-            buildOutputs(outputs, results);
+        if (r != NULL) {
+            outputs = r->getOutputs();
             return true;
         }
     }
