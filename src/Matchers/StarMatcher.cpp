660b397197a173bb4021615152111f9feb71b63f
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-05-24 11:40:32 +0200

    Make StarMatcher inherit from RangeMatcher

--- src/Matchers/StarMatcher.cpp
+++ src/Matchers/StarMatcher.cpp
@@ -1,54 +1,9 @@
 #include "StarMatcher.hpp"
 
-StarMatcher::StarMatcher(MatcherInterface *m)
+StarMatcher::StarMatcher(MatcherInterface *m) : RangeMatcher(m)
 {
-    matcher = m;
 }
 
 StarMatcher::~StarMatcher()
 {
-}
-
-Result* StarMatcher::match(
-    const vector<MatchableInterface *> &matchables,
-    size_t start,
-    const forward_list<Result> &previousResults
-) {
-    if (start >= matchables.size()) {
-        return NULL;
-    }
-    results = stack<Result*>();
-    results.push(new Result(start - 1, start - 1));
-    for (size_t i = start; i < matchables.size(); ++i) {
-        Result* r = matcher->match(matchables, i, previousResults);
-        if (r == NULL) {
-            break;
-        }
-        while (r != NULL) {
-            results.push(new Result(
-                start,
-                r->getLastMatchedIndex(),
-                r->getOutputs()
-            ));
-            r = matcher->next();
-        }
-    }
-    return next();
-}
-
-Result* StarMatcher::match(
-    const vector<MatchableInterface *> &matchables,
-    size_t start
-) {
-    return match(matchables, start, forward_list<Result>{});
-}
-
-Result* StarMatcher::next()
-{
-    if (results.empty()) {
-        return NULL;
-    }
-    Result* r = results.top();
-    results.pop();
-    return r;
 }
--- src/Matchers/StarMatcher.hpp
+++ src/Matchers/StarMatcher.hpp
@@ -1,31 +1,10 @@
 #pragma once
-#include <string>
-#include <forward_list>
-#include <stack>
-#include "../Result.hpp"
-#include "../Matchables/MatchableInterface.hpp"
+#include "RangeMatcher.hpp"
 #include "MatcherInterface.hpp"
 
-class StarMatcher: public MatcherInterface
+class StarMatcher: public RangeMatcher
 {
     public:
         StarMatcher(MatcherInterface *m);
         ~StarMatcher();
-
-        Result* match(
-            const vector<MatchableInterface *> &matchables,
-            size_t start,
-            const forward_list<Result> &previousResults
-        );
-
-        Result* match(
-            const vector<MatchableInterface *> &matchables,
-            size_t start
-        );
-
-        Result* next();
-
-    protected:
-        MatcherInterface *matcher;
-        stack<Result*> results;
 };
