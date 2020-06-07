8710fa82bac4f13385078983d3e9874db670aef8
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-06-07 16:49:56 +0200

    Add test for propagating multiple results of each matcher

--- test/Matchers/OrMatcherTest.cpp
+++ test/Matchers/OrMatcherTest.cpp
@@ -1,6 +1,7 @@
 #include "../catch.hpp"
 #include "../../src/Matchables/StringMatchable.hpp"
 #include "../../src/Matchers/OrMatcher.hpp"
+#include "../../src/Matchers/StarMatcher.hpp"
 #include "../../src/Matchers/StringMatcher.hpp"
 #include "../../src/Result.hpp"
 
@@ -8,6 +9,7 @@ TEST_CASE("OrMatcher matches any of the specified submatchers in order")
 {
     StringMatchable a("a"), b("b"), c("c");
     StringMatcher ma("a"), mb("b"), mc("c");
+    StarMatcher sa(&ma), sb(&mb), sc(&mc);
     vector<MatchableInterface *> input{&a, &b, &c, &c};
 
     SECTION("Not matches sequence at the end")
@@ -25,6 +27,18 @@ TEST_CASE("OrMatcher matches any of the specified submatchers in order")
         REQUIRE(*(m.match(input, 1)) == Result(1, 1));
         REQUIRE(m.next() == NULL);
         REQUIRE(*(m.match(input, 2)) == Result(2, 2));
+        REQUIRE(m.next() == NULL);
+    }
+
+    SECTION("Next matches other matchers in the list, in order")
+    {
+        OrMatcher m(vector<MatcherInterface*>{&sc, &sc});
+        REQUIRE(*(m.match(input, 2)) == Result(2, 3));
+        REQUIRE(*(m.next()) == Result(2, 2));
+        REQUIRE(*(m.next()) == Result(1, 1));
+        REQUIRE(*(m.next()) == Result(2, 3));
+        REQUIRE(*(m.next()) == Result(2, 2));
+        REQUIRE(*(m.next()) == Result(1, 1));
         REQUIRE(m.next() == NULL);
     }
 }
