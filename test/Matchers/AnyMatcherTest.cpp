2d37ce632bf9d6641cf4f9f6ce3ab4e0e30db022
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-06-20 17:26:02 +0200

    Add AnyMatcher

---
+++ test/Matchers/AnyMatcherTest.cpp
@@ -0,0 +1,35 @@
+#include "../catch.hpp"
+#include "../../src/Matchers/AnyMatcher.hpp"
+#include "../../src/Matchables/StringMatchable.hpp"
+#include "../../src/Result.hpp"
+
+TEST_CASE("AnyMatcher matches only with beginning of sequence")
+{
+    StringMatchable a("a"), b("b"), c("c");
+    AnyMatcher m;
+    vector<MatchableInterface *> input{&a, &b, &c, &c};
+
+    SECTION("Result an element from the beginning")
+    {
+        REQUIRE(*(m.match(input, 0)) == Result(0, 0));
+        REQUIRE(m.next() == NULL);
+    }
+
+    SECTION("Result of matching the second element")
+    {
+        REQUIRE(*(m.match(input, 1)) == Result(1, 1));
+        REQUIRE(m.next() == NULL);
+    }
+
+    SECTION("Not matches sequence at the end")
+    {
+        REQUIRE(m.match(input, input.size()) == NULL);
+        REQUIRE(m.next() == NULL);
+    }
+
+    SECTION("Not matches sequence after end")
+    {
+        REQUIRE(m.match(input, input.size() + 1) == NULL);
+        REQUIRE(m.next() == NULL);
+    }
+}
