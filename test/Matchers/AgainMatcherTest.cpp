86ce0b6ad067694a6af764b892b9487ef6e0b727
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-06-21 17:04:24 +0200

    Add a non referenced group to check it skips it

--- test/Matchers/AgainMatcherTest.cpp
+++ test/Matchers/AgainMatcherTest.cpp
@@ -21,6 +21,9 @@ TEST_CASE("AgainMatcher matches only with same strings")
     SECTION("Matches if a result exists with the same sequence")
     {
         forward_list<Result> prevResults = {
+            Result(0, 3, unordered_map<string, forward_list<MatchableInterface*>>{
+                {"other", forward_list<MatchableInterface*>{&a, &b, &c, &a}}
+            }),
             Result(0, 2, unordered_map<string, forward_list<MatchableInterface*>>{
                 {"repeat", forward_list<MatchableInterface*>{&a, &b, &c}}
             })
