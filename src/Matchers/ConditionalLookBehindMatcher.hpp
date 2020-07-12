1f00e8e3a939836ec2e6357c9d3fc87751c94559
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-07-12 12:47:06 +0200

    Remove unused includes

--- src/Matchers/ConditionalLookBehindMatcher.hpp
+++ src/Matchers/ConditionalLookBehindMatcher.hpp
@@ -1,8 +1,4 @@
 #pragma once
-#include <string>
-#include <forward_list>
-#include "../Result.hpp"
-#include "../Matchables/MatchableInterface.hpp"
 #include "ConditionalLookAheadMatcher.hpp"
 
 class ConditionalLookBehindMatcher: public ConditionalLookAheadMatcher
