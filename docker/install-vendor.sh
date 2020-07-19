ea4c18602b952782246d98130dee91733f9aa5cb
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-07-19 20:03:55 +0200

    Fix chown command

--- docker/install-vendor.sh
+++ docker/install-vendor.sh
@@ -11,6 +11,7 @@ echo "Installing Boost library (https://www.boost.org) ..."
 wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2
 tar --bzip2 -xvf boost_1_73_0.tar.bz2
 rm -rf boost_1_73_0.tar.bz2
-chown -R boost_1_73_0 --reference=test
-apt-get install -y dladdr
 echo "... Boost library installed successfully!"
+
+cd BASEPATH/..
+chown -R vendor --reference=test
