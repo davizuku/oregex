995184d6cc1ed9ada25819e3ad8934ebc943647d
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-07-19 20:04:16 +0200

    Remove verbosity from tar command

--- docker/install-vendor.sh
+++ docker/install-vendor.sh
@@ -9,7 +9,7 @@ cd vendor
 # Boost library
 echo "Installing Boost library (https://www.boost.org) ..."
 wget https://dl.bintray.com/boostorg/release/1.73.0/source/boost_1_73_0.tar.bz2
-tar --bzip2 -xvf boost_1_73_0.tar.bz2
+tar --bzip2 -xf boost_1_73_0.tar.bz2
 rm -rf boost_1_73_0.tar.bz2
 echo "... Boost library installed successfully!"
 
