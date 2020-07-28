bf9f37b773bdaf29f5fee6f8cac6decd7381dafe
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-07-28 19:30:23 +0200

    Fix leaks in NegativeLookAheadMatcher

--- src/Matchers/NegativeLookAheadMatcher.cpp
+++ src/Matchers/NegativeLookAheadMatcher.cpp
@@ -1,7 +1,19 @@
 #include "NegativeLookAheadMatcher.hpp"
 #include "ExceptMatcher.hpp"
 
-NegativeLookAheadMatcher::NegativeLookAheadMatcher(MatcherInterface *matcher) :
-    PositiveLookAheadMatcher(new ExceptMatcher(vector<MatcherInterface *>{matcher})) {}
+Result *NegativeLookAheadMatcher::match(
+    const vector<MatchableInterface *> &matchables,
+    size_t start,
+    const forward_list<Result> &previousResults
+) {
+    ExceptMatcher em(vector<MatcherInterface *>{this->matcher});
+    this->matcher = &em;
+    return PositiveLookAheadMatcher::match(matchables, start, previousResults);
+}
 
-NegativeLookAheadMatcher::~NegativeLookAheadMatcher() {}
+Result *NegativeLookAheadMatcher::match(
+    const vector<MatchableInterface *> &matchables,
+    size_t start
+) {
+    return match(matchables, start, forward_list<Result>{});
+}
