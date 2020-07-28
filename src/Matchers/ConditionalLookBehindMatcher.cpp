f45114e2cf48ca8c79a56e3ebea88926cce02554
David Álvarez Pons <david_alvarezpons@hotmail.com>
2020-07-28 19:38:12 +0200

    Fix leaks in ConditionalLookBehindMatcher

--- src/Matchers/ConditionalLookBehindMatcher.cpp
+++ src/Matchers/ConditionalLookBehindMatcher.cpp
@@ -1,16 +1,19 @@
 #include "ConditionalLookBehindMatcher.hpp"
 #include "PositiveLookBehindMatcher.hpp"
 
-ConditionalLookBehindMatcher::ConditionalLookBehindMatcher(
-    MatcherInterface* condMatcher,
-    MatcherInterface* trueMatcher,
-    MatcherInterface* falseMatcher
-) : ConditionalLookAheadMatcher(
-    new PositiveLookBehindMatcher(condMatcher),
-    trueMatcher,
-    falseMatcher
-) {}
-
-ConditionalLookBehindMatcher::~ConditionalLookBehindMatcher()
+Result *ConditionalLookBehindMatcher::match(
+    const vector<MatchableInterface *> &matchables,
+    size_t start,
+    const forward_list<Result> &previousResults)
 {
+    PositiveLookBehindMatcher pbm(condMatcher);
+    condMatcher = &pbm;
+    return ConditionalLookAheadMatcher::match(matchables, start, previousResults);
+}
+
+Result *ConditionalLookBehindMatcher::match(
+    const vector<MatchableInterface *> &matchables,
+    size_t start
+) {
+    return match(matchables, start, forward_list<Result>{});
 }
